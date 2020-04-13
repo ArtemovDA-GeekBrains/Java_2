ddde01e55fad59db71a34b459e01abe10b29266d
ArtemovDA-GeekBrains <artemov14@mail.ru>
2020-04-13 15:13:54 +0300

    Не разобрался как сделать отдельные методы для вывода телефона и e-mail (выводятся совместно).

---
+++ lesson_3/Person.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.java_2.hw_3;
+
+import java.util.ArrayList;
+
+public class Person {
+    String name;
+    String phone;
+    String email;
+
+    public Person(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+}
